037517481aca1c3a56903f95f9ce1f7fd4322e68
Mitchell <mitchellm27@gmail.com>
2022-05-01 22:55:32 -0700

    adjusted starting code.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import Header from "./components/Header";
 import Section from "./components/Section";
-import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
-      <Navbar />
       <Header />
       <Section />
     </div>
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,12 +1,5 @@
 import React from 'react';
-import '../styles/Section.css';
 
-// By importing the Section.css file, it is added to the DOM whenever this component loads
-
-// We can also style a component inside of its JavaScript file by adding style properties to its rendered elements
-// Unlike regular HTML, a JSX style property must be an object instead of a string
-// On a style object, we camelCase all property names, and put all of the values in quotes
-// Non quoted values default to "pixels", e.g. height, margin, padding
 
 const styles = {
   sectionStyles: {
@@ -14,11 +7,10 @@ const styles = {
   },
 };
 
-// We use JSX curly braces to evaluate the style object on the JSX tag
 
 function Section() {
   return (
-    <section style={styles.sectionStyles} className="section">
+    <section style={styles.sectionStyles} className="section card">
       <h2>Lorem Ipsum Dolor Sit Amet</h2>
       <p>
         Sed ut perspiciatis unde omnis iste natus error sit voluptatem
