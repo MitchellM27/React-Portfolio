a4ccbd978b6cd8c8e3d4435721811a3a65b752df
Mitchell <mitchellm27@gmail.com>
2022-05-05 13:38:02 -0700

    added gh pages functinality.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,42 @@
 import React from 'react';
 
 const styles = {
-  headerStyle: {
-    background: '#75ABBC',
-    color: "#071013"
+  // headerStyle: {
+  //   background: '#75ABBC',
+  //   color: "#071013"
+  // },
+  // headingStyle: {
+  //   fontSize: '35px',
+  // },
+  header: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: "1%",
   },
-  headingStyle: {
-    fontSize: '35px',
-  },
+
+  navList: {
+    display: "flex",
+    flexDirection: "horizontal",
+    justifyContent: "space-between",
+    minWidth: "275px",
+    listStyle: "none",
+  }
 };
 
 
 function Header() {
   return (
-    <header style={styles.headerStyle} className="header">
-      <h1 style={styles.headingStyle}>Mitchell Mudgett</h1>
+    <header class="nav">
+        <h1 class="nav-item">Mitchell Mudgett</h1>
+        <nav class="nav-item" >
+          <ul class = "navList">
+            <li class="p-3"><a href="#about-me">About Me</a></li>
+            <li class="p-3"><a href="#work">Work</a></li>
+            <li class="p-3"><a href="#contact-me">Contact Me</a></li>
+            <li class="p-3"><a href="Images/resume.PDF">Resume</a></li>
+          </ul>
+        </nav>
     </header>
   );
 }
