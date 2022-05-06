b47a9e767e48f491e5fa90707110470b38931e46
Mitchell <mitchellm27@gmail.com>
2022-05-05 23:29:16 -0700

    Updates

---
+++ precache-manifest.32c21b5404f224c5f17d81c5431b0bfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "093fb52cc027e9dd0b33",
+    "url": "/React-Portfolio/static/js/main.093fb52c.chunk.js"
+  },
+  {
+    "revision": "25779be3bf3e3a1e0db2",
+    "url": "/React-Portfolio/static/css/1.e41cbd58.chunk.css"
+  },
+  {
+    "revision": "25779be3bf3e3a1e0db2",
+    "url": "/React-Portfolio/static/js/1.25779be3.chunk.js"
+  },
+  {
+    "revision": "804d233a10add83efa8d",
+    "url": "/React-Portfolio/static/js/runtime~main.804d233a.js"
+  },
+  {
+    "revision": "70e28b94b4e133d7035ee7557b2baa09",
+    "url": "/React-Portfolio/static/media/resume.70e28b94.PDF"
+  },
+  {
+    "revision": "d19452f0e4c85d824b0b2ae690e219bb",
+    "url": "/React-Portfolio/index.html"
+  }
+];
