a8ca3a583e5f135f877002f48ebeb1fb8f378278
Mitchell <mitchellm27@gmail.com>
2022-05-08 10:36:21 -0700

    adjusted projects page.

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -17,6 +17,16 @@ const styles = {
   projectsContainer: {
     flexDirection: "horizontal",
     flex: "wrap"
+  },
+  box: {
+    height:"250px",
+    width:"250px",
+    display:"block",
+    marginBottom:"15px",
+    border: "2px",
+    borderStyle: "solid",
+    borderColor: "black",
+    alignItems: "center"
   }
 
 
@@ -31,42 +41,49 @@ function Project() {
       </section>
 
       <section>
-        <div style= {styles.projectsContainer} className="projectsContainer container d-flex justify-content-around">
-          <div className='row align-items-center justify-content-around col-8'>
-            <div className='projectOne col'>
-              <div>
-                <h4 className='row'>Text Editor</h4>
-                <a className='row' href='https://github.com/MitchellM27/Text-Editor'>Github</a>
-                <a className='row' href='https://hidden-plateau-11846.herokuapp.com/'>Live Link</a>
-              </div>
-            </div>
-            <div className='projectTwo col'>
-                <h4 className='row'>Social Network API</h4>
-                <a className='row' href='https://github.com/MitchellM27/Social-Network-API'>Github</a>
-            </div>
-            <div className='projectThree col'>
-                <h4 className='row'>Tech Blog</h4>
-                <a className='row' href='https://github.com/MitchellM27/Tech-Blog'>Github</a>
-                <a className='row' href='https://serene-atoll-54262.herokuapp.com/'>Live Link</a>
-            </div>
-          </div>
-          <div className='row align-items-center justify-content-around col-8'>
-            <div className='projectFour col'>
-                <h4 className='row'>E-Commerce Back End</h4>
-                <a className='row' href='https://github.com/MitchellM27/E-Commerce-Back-End'>Github</a>
-            </div>
-            <div className='projectFive col'>
-                <h4 className='row'>Stock Finder Application</h4>
-                <a className='row' href='https://github.com/MitchellM27/Stock-Finder-Application'>Github</a>
-                <a className='row' href='https://mitchellm27.github.io/Stock-Finder-Application/'>Live Link</a>
-            </div>
-            <div className='projectSix col'>
-                <h4 className='row'>Work Day Scheduler</h4>
-                <a className='row' href='https://github.com/MitchellM27/Work-Day-Scheduler'>Github</a>
-                <a className='row' href='https://mitchellm27.github.io/Work-Day-Scheduler/'>Live Link</a>
-            </div>
-          </div>
-        </div>
+      <div class="row"> 
+     <div class="col-md-4 col-sm-6">
+        <span class="box" className='row' style={styles.box}>
+          <h4 className='row my-auto'>Text Editor</h4>
+          <a className='row' href='https://github.com/MitchellM27/Text-Editor'>Github</a>
+          <a className='row' href='https://hidden-plateau-11846.herokuapp.com/'>Live Link</a>
+				</span>
+    </div>
+    <div class="col-md-4 col-sm-6">
+      <span class="box" style={styles.box}>
+        <h4 className='row my-auto'>Social Network API</h4>
+        <a className='row' href='https://github.com/MitchellM27/Social-Network-API'>Github</a>
+			</span>
+    </div>
+    <div class="col-md-4 col-sm-6">
+       <span class="box" style={styles.box}>
+         <h4 className='row'>Tech Blog</h4>
+         <a className='row' href='https://github.com/MitchellM27/Tech-Blog'>Github</a>
+         <a className='row' href='https://serene-atoll-54262.herokuapp.com/'>Live Link</a>		
+				</span>
+    </div>
+
+    <div class="col-md-4 col-sm-6">
+        <span class="box" style={styles.box}>
+          <h4 className='row'>E-Commerce</h4>
+          <a className='row' href='https://github.com/MitchellM27/E-Commerce-Back-End'>Github</a>
+				</span>
+    </div>
+    <div class="col-md-4 col-sm-6">
+        <span class="box" style={styles.box}>
+          <h4 className='row'>Stock Finder App</h4>
+          <a className='row' href='https://github.com/MitchellM27/Stock-Finder-Application'>Github</a>
+          <a className='row' href='https://mitchellm27.github.io/Stock-Finder-Application/'>Live Link</a>
+				</span>
+    </div>
+    <div class="col-md-4 col-sm-6">
+        <span class="box" style={styles.box}>
+          <h4 className='row'>Work Day Scheduler</h4>
+          <a className='row' href='https://github.com/MitchellM27/Work-Day-Scheduler'>Github</a>
+          <a className='row' href='https://mitchellm27.github.io/Work-Day-Scheduler/'>Live Link</a>
+				</span>
+    </div>
+</div>
       </section>
     </section>
   );
